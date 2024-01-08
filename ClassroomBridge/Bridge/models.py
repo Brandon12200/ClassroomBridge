7a02298028eb527b7d4f9aedffc234876a552981
Brandon12200 <BrandonJKenney1@gmail.com>
2024-01-08 01:25:22 -0500

    Worked on index page and added User model

--- ClassroomBridge/Bridge/models.py
+++ ClassroomBridge/Bridge/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    id = models.AutoField(primary_key=True)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
